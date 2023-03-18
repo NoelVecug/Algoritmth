febaf2658b983f73082101e014d0b72649a45567
NoelVecug <geesegagaga@mail.ru>
2023-03-18 14:56:11 +0300

    Fibonacci

---
+++ src/DigitOfFibonachy/Main.java
@@ -0,0 +1,19 @@
+package DigitOfFibonachy;
+public class Main {
+    private static final int CONST = 10;
+    public static void main(String[] args) {
+        System.out.println(calculate(CONST));
+    }
+    private static long calculate(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        long[] array = new long[n + 1];
+        array[0] = 0;
+        array[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            array[i] = array[i-1] + array[i-2];
+        }
+        return array[n];
+    }
+}
